18b2d9c492c395c95137861f3f45614634288a9f
Noah Sandler <noahsandler@gmail.com>
2022-01-06 17:56:09 -0500

    Implement Ghost.get_valid_moves

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -13,14 +13,33 @@ public class Ghost{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;
+		ArrayList<Location> valid_moves = new ArrayList<Location>();
+
+		/* determime neighbors in all 4 possible directions */
+		valid_moves.add(myLoc.shift(0, 1));
+		valid_moves.add(myLoc.shift(1, 0));
+		valid_moves.add(myLoc.shift(0, -1));
+		valid_moves.add(myLoc.shift(-1, 0));
+
+		/*
+		* figure out which neighboring moves are invalid and remove them from valid_moves.
+		* an invalid GHOST move is one where the location is occupied by a wall.
+		*/
+		for (Location neighbor : valid_moves) {
+			HashSet<Map.Type> contents = myMap.getLoc(neighbor);
+			Boolean condition = contents == null ||
+				contents.contains(Map.Type.WALL);
+			valid_moves.removeIf(ele -> condition);
+		}
+
+		return valid_moves;
 	}
 
 	public boolean move() {
 		return false;
 	}
 
-	public boolean is_pacman_in_range() { 
+	public boolean is_pacman_in_range() {
 		return false;
 	}
 
