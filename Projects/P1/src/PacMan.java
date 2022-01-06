4cf8528b6a10f11f158514d8ab54ec41e72f4cce
Noah Sandler <noahsandler@gmail.com>
2022-01-06 16:49:45 -0500

    implement PacMan.get_valid_moves

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -6,7 +6,7 @@ public class PacMan{
 	String myName;
 	Location myLoc;
 	Map myMap;
-	Location shift; 
+	Location shift;
 
 	public PacMan(String name, Location loc, Map map) {
 		this.myLoc = loc;
@@ -15,18 +15,38 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		ArrayList<Location> valid_moves = new ArrayList<Location>();
+
+		/* determime neighbors in all 4 possible directions */
+		valid_moves.add(myLoc.shift(0, 1));
+		valid_moves.add(myLoc.shift(1, 0));
+		valid_moves.add(myLoc.shift(0, -1));
+		valid_moves.add(myLoc.shift(-1, 0));
+
+		/*
+		* figure out which neighboring moves are invalid and remove them from valid_moves.
+		* an invalid move is one where the location is occupied by a wall or a ghost.
+		*/
+		for (Location neighbor : valid_moves) {
+			HashSet<Map.Type> contents = myMap.getLoc(neighbor);
+			Boolean condition = contents == null ||
+				contents.contains(Map.Type.GHOST) ||
+				contents.contains(Map.Type.WALL);
+			valid_moves.removeIf(ele -> condition);
+		}
+
+		return valid_moves;
 	}
 
 	public boolean move() {
 		return false;
 	}
 
-	public boolean is_ghost_in_range() { 
+	public boolean is_ghost_in_range() {
 		return false;
 	}
 
-	public JComponent consume() { 
+	public JComponent consume() {
  		return null;
 	}
 }
