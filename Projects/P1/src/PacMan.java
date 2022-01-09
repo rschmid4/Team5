fc40657f969aeab76392fb35b08ea30546fffd8c
Aditi Sekar <asekar@umd.edu>
2022-01-08 22:01:42 -0500

    Pacman move

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -15,11 +15,44 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+	ArrayList<Location> valid_moves = new ArrayList<Location>();
+		
+		
+		
+		/* determime neighbors in all 4 possible directions */
+		Location up = new Location(myLoc.x, myLoc.y-1);
+		Location down =  new Location(myLoc.x, myLoc.y+1);
+		Location right =  new Location(myLoc.x+1, myLoc.y);
+		Location left =  new Location(myLoc.x-1, myLoc.y);
+		
+		
+		
+		if(myMap.getLoc(up) == null || myMap.getLoc(up).size() == 0){
+			valid_moves.add(up);
+		}
+		if(myMap.getLoc(down) == null ||myMap.getLoc(down).size() == 0){
+			valid_moves.add(down);
+		}
+		if(myMap.getLoc(left) == null ||myMap.getLoc(left).size() == 0){
+			valid_moves.add(left);
+		}
+		if(myMap.getLoc(right) == null ||myMap.getLoc(right).size() == 0){
+			valid_moves.add(right);
+		}
+		
+		return valid_moves;
 	}
 
 	public boolean move() {
-		return false;
+	ArrayList <Location> valid_moves = get_valid_moves();
+		int numMoves = valid_moves.size();
+		if(numMoves != 0) {
+			int randomLoc = (int) (Math.random()* (numMoves - 1));
+			return myMap.move(myName, (Location)valid_moves.get(randomLoc), Map.Type.PACMAN);
+		} else {
+			return false;
+		}
+		
 	}
 
 	public boolean is_ghost_in_range() { 
