6b876c45a21ca85bf21b6ab04c8399f09b227bd9
Aditi Sekar <asekar@umd.edu>
2022-01-11 14:14:35 -0500

    fix pacman move junit test

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -9,6 +9,30 @@ public class TestPacManMove extends TestCase {
 	Map mp;
 
 	public void testPacManMove(){
+		NoFrame frame = new NoFrame();
+		mp = new Map(10);
+		lc = new Location(5,5);
+		pm = new PacMan("Bob", lc , mp);
+
+		Location up = lc.shift(0, 1); 
+		Location down = lc.shift(0, -1); 
+		Location right = lc.shift(1, 0); 
+		Location left = lc.shift(-1, 0);
+		
+		Map grid = frame.getMap(); 
+		frame.addPacMan(lc);
+
+		GhostComponent gc = new GhostComponent(up.x, up.y, 10);
+		GhostComponent gc2 = new GhostComponent(right.x, right.y, 10);
+
+		mp.add("Pam", up, gc, Map.Type.GHOST);
+		mp.add("Dave", right, gc2, Map.Type.GHOST);
+
+		assertTrue(pm.move());
+	}
+
+	public void testPacManCantMove(){
+		NoFrame frame = new NoFrame();
 		mp = new Map(10);
 		lc = new Location(5,5);
 		pm = new PacMan("Bob", lc , mp);
@@ -18,26 +42,8 @@ public class TestPacManMove extends TestCase {
 		Location right = lc.shift(1, 0); 
 		Location left = lc.shift(-1, 0); 
 
-		GhostComponent gc = new GhostComponent(up.x, up.y, 10);
-		GhostComponent gc2 = new GhostComponent(right.x, right.y, 10);
-		GhostComponent gc3 = new GhostComponent(left.x, left.y, 10);
-
-		mp.add("Pam", up, gc, Map.Type.GHOST);
-		mp.add("Dave", right, gc2, Map.Type.GHOST);
-		mp.add("Jim", left, gc3, Map.Type.GHOST);
-
-		assertTrue(pm.move());
-	}
-
-	public void testPacManCantMove(){
-	mp = new Map(10);
-		lc = new Location(5,5);
-		pm = new PacMan("Bob", lc , mp);
-
-		Location up = lc.shift(0, 1); 
-		Location down = lc.shift(0, -1); 
-		Location right = lc.shift(1, 0); 
-		Location left = lc.shift(-1, 0); 
+		Map grid = frame.getMap();
+                frame.addPacMan(lc);
 
 		GhostComponent gc = new GhostComponent(up.x, up.y, 10);
 		GhostComponent gc1 = new GhostComponent(down.x, down.y, 10);
