2978a9891dc1cbfd2ad68823bb41c6aa098ba03d
Eric Chu <echu123@umd.edu>
2022-01-11 20:11:23 -0500

    fixed 3 tests among 3 classes

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -1,34 +1,35 @@
-import java.awt.Color;
 import junit.framework.*;
+import java.awt.Color;
+import java.io.*;
 
-public class TestMapAttack {
+public class TestMapAttack extends TestCase{
+
+	PacMan myPacman;
+	Map myMap;
+	Ghost myGhost;
+	Location loc1; 
 
 	public void testMapAttack() {
 
-		mp = new Map(100);
-		lc = new Location(22,34);
-		pm = new PacMan("Bob", lc , mp);
-		ghost = new Ghost("Caspar", lc, mp);
-		
-		Location up = lc.shift(0, 1); 
-		Location down = lc.shift(0, -1); 
-		Location right = lc.shift(1, 0); 
-		Location left = lc.shift(-1, 0); 
-		
-		GhostComponent gc = new GhostComponent(up.x, up.y, 10);
-		PacManComponent pc = new PacManComponent(down.x, down.y, 10);
-		GhostComponent gc2 = new GhostComponent(right.x, right.y, 10);
-		
-		mp.add("Pam", up, gc, Map.Type.GHOST);
-		mp.add("Bob", down, pc, Map.Type.PACMAN);
+		// Create my location starters
+   		loc1 = new Location(5,6);
+		Location loc2 = new Location(6,6);
 
-		assertTrue(mp.attack("Caspar"));
+		// Create my playing field. 
+		myMap = new Map(10);
+		myGhost = new Ghost("Caspar", loc1, myMap);
+
+		// Add Components 
+		GhostComponent gcomponent = new GhostComponent(5, 6, 10);
+		PacManComponent pcomponent = new PacManComponent(6, 6, 10);
+
+		// Add to map my Ghost + PacMan
+		myMap.add("Caspar", loc1, gcomponent, Map.Type.GHOST);
+		myMap.add("Manny", loc2, pcomponent, Map.Type.PACMAN);
+
+		// Check if the PacMan is right next to the homeboy Caspar 
+		assertTrue(myMap.attack("Caspar"));
 
 	}
-
-
-
-
-
 
 }
--- Projects/P1/tests/TestPacManInRange.java
+++ Projects/P1/tests/TestPacManInRange.java
@@ -2,38 +2,56 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-
-@Test
 public class TestPacManInRange extends TestCase {
+ 
+   PacMan myPacman;
+   Map myMap;
+   Ghost myGhost;
+   Location loc1; 
+ 
+   public void testPacManInRange() {
+	   
+	   loc1 = new Location(5,6);
+	   Location loc2 = loc1.shift(1,0);
 
-	PacMan pm;
-	Location lc;
-	Map mp;
-	Ghost ghost;
+	   // Create my playing field. 
+       myMap = new Map(100);
+	   myPacman = new PacMan("Manny", loc1, myMap);
+       myGhost = new Ghost("Caspar", loc2, myMap);
 
-	public void testPacManInRange() {
+		// Add Components 
+		GhostComponent gcomponent = new GhostComponent(6, 6, 10);
+		PacManComponent pcomponent = new PacManComponent(5 ,6, 10);
 
-		mp = new Map(10);
-		lc = new Location(5,5);
-		pm = new PacMan("Bob", lc , mp);
-		ghost = new Ghost("Pam", lc, mp);
+		// Add to map 
+		myMap.add("Caspar", loc2, gcomponent, Map.Type.GHOST);
+		myMap.add("Manny", loc1, pcomponent, Map.Type.PACMAN);
 		
-		Location up = lc.shift(0, 1); 
-		Location down = lc.shift(0, -1); 
-		Location right = lc.shift(1, 0); 
-		Location left = lc.shift(-1, 0); 
-		
-		GhostComponent gc = new GhostComponent(up.x, up.y, 10);
-		PacManComponent pc = new PacManComponent(down.x, down.y, 10);
-		GhostComponent gc2 = new GhostComponent(right.x, right.y, 10);
-		WallComponent wc = new WallComponent(left.x, left.y, 10);
-		
-		mp.add("Pam", up, gc, Map.Type.GHOST);
-		mp.add("Bob", down, pc, Map.Type.PACMAN);
-		mp.add("Jim", left, wc, Map.Type.WALL);
-		
-		
-		assertTrue(pm.is_ghost_in_range());
+		// Check if the Ghost is right next to the homeboy Manny 
+      	assertTrue(myPacman.is_ghost_in_range());
 
-	}
+ 	}
+
+	public void testPacManIsNotRange() {
+	   
+	   loc1 = new Location(405, 63);
+	   Location loc2 = loc1.shift(10,4);
+
+	   // Create my playing field. 
+       myMap = new Map(500);
+	   myPacman = new PacMan("Manny", loc1, myMap);
+       myGhost = new Ghost("Caspar", loc2, myMap);
+
+	   // Add Components 
+		GhostComponent gcomponent = new GhostComponent(415, 67, 10);
+		PacManComponent pcomponent = new PacManComponent(405, 63, 10);
+
+		// Add to map 
+		myMap.add("Caspar", loc2, gcomponent, Map.Type.GHOST);
+		myMap.add("Manny", loc1, pcomponent, Map.Type.PACMAN);
+		
+		// Check if the Ghost is right next to the homeboy Manny 
+      	assertFalse(myPacman.is_ghost_in_range());
+
+ 	}
 }
