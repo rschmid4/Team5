5d8e8f664f2ff060ca03ed3a1755933d0d6508d7
Hank Wu <cwu@terpmail.umd.edu>
2022-01-14 00:29:32 -0500

    Updated PacMan consume to fail my tests

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -74,10 +74,12 @@ public class PacMan{
 		Location loc = new Location(this.myLoc.x, this.myLoc.y);
 
 		if (this.myMap.getLoc(loc).contains(Map.Type.COOKIE)) {
-			return this.myMap.eatCookie(this.myName);
+			// return this.myMap.eatCookie(this.myName);
+			return null;
 		}
 
- 		return null;
+		GhostComponent gc = new GhostComponent(5, 5, 10);
+ 		return gc;
 
 	}
 }
