81202a63f0d9f2301662b10c7c4ba8f088a4af72
Eric Chu <echu123@umd.edu>
2022-01-15 03:01:05 -0500

    moving p1 to p2

---
+++ Projects/P2/src/PacMan.java
@@ -0,0 +1,78 @@
+import java.util.HashSet;
+import java.util.ArrayList;
+import javax.swing.JComponent;
+
+public class PacMan{
+	String myName;
+	Location myLoc;
+	Map myMap;
+	Location shift;
+
+	public PacMan(String name, Location loc, Map map) {
+		this.myLoc = loc;
+		this.myName = name;
+		this.myMap = map;
+	}
+
+	public ArrayList<Location> get_valid_moves() {
+		ArrayList<Location> valid_moves = new ArrayList<Location>();
+
+		Location up = this.myLoc.shift(0, 1);
+		Location down = this.myLoc.shift(0, -1);
+		Location right = this.myLoc.shift(1, 0);
+		Location left = this.myLoc.shift(-1, 0);
+
+		if (!this.myMap.getLoc(right).contains(Map.Type.WALL)) {
+			valid_moves.add(right);
+		}
+		if (!this.myMap.getLoc(left).contains(Map.Type.WALL)) {
+			valid_moves.add(left);
+		}
+		if (!this.myMap.getLoc(up).contains(Map.Type.WALL)) {
+			valid_moves.add(up);
+		}
+		if (!this.myMap.getLoc(down).contains(Map.Type.WALL)) {
+			valid_moves.add(down);
+		}
+		return valid_moves;
+
+	}
+
+	public boolean move() {
+		ArrayList <Location> valid_moves = get_valid_moves();
+		int numMoves = valid_moves.size();
+		if(numMoves != 0) {
+			int randomLoc = (int) (Math.random()* (numMoves));
+			this.myMap.move(myName, (Location)valid_moves.get(randomLoc), Map.Type.PACMAN);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean is_ghost_in_range() {
+		Location up = this.myLoc.shift(0, 1);
+		Location down = this.myLoc.shift(0, -1);
+		Location right = this.myLoc.shift(1, 0);
+		Location left = this.myLoc.shift(-1, 0);
+		Boolean condition;
+
+		condition = this.myMap.getLoc(up).contains(Map.Type.GHOST) ||
+					this.myMap.getLoc(down).contains(Map.Type.GHOST) ||
+					this.myMap.getLoc(left).contains(Map.Type.GHOST) ||
+					this.myMap.getLoc(right).contains(Map.Type.GHOST);
+
+		return condition;
+	}
+
+	public JComponent consume() {
+		Location loc = new Location(this.myLoc.x, this.myLoc.y);
+
+		if (this.myMap.getLoc(loc).contains(Map.Type.COOKIE)) {
+			return this.myMap.eatCookie(this.myName);
+		}
+
+ 		return null;
+
+	}
+}
---
+++ Projects/P2/tests/TestConsume.java
@@ -0,0 +1,37 @@
+import junit.framework.*;
+import java.awt.Color;
+import java.io.*;
+import java.util.*;
+
+import javax.swing.JComponent;
+
+public class TestConsume extends TestCase {
+
+	public void testConsume() {
+
+
+		Map m = new Map(10);
+		Location loc = new Location(5, 5);
+		PacMan p = new PacMan("pacman", loc, m);
+		PacManComponent p1 = new PacManComponent(5, 5, 10);
+		CookieComponent cookie = new CookieComponent(5, 5, 10);
+
+		m.add("tok_x5_y5", loc, cookie, Map.Type.COOKIE);
+		m.add("pacman", loc, p1, Map.Type.PACMAN);
+
+
+		assertTrue(p.consume().equals(cookie));
+	}
+
+	public void testConsumeFail() {
+		Map m = new Map(10);
+		PacMan p = new PacMan("pacman", new Location(3, 2), m);
+		PacManComponent p1 = new PacManComponent(3, 2, 10);
+		CookieComponent cookie = new CookieComponent(5, 5, 10);
+
+		m.add("tok_x5_y5", new Location(5, 5), cookie, Map.Type.COOKIE);
+		m.add("pacman", new Location(3, 2), p1, Map.Type.PACMAN);
+
+		assertTrue(p.consume() == null);
+	}
+}
