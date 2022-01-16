d91af4e116c7a7d9f68e3a0ac61dc2463e843f45
Hank Wu <cwu@terpmail.umd.edu>
2022-01-16 14:17:22 -0500

    Added headless for main yml

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -15,22 +15,4 @@ public class TestPacManMove extends TestCase {
                 mp.add("Aditi", pacLoc, pacComp, Map.Type.PACMAN);
                 assertTrue(pm.move());
         }
-
-        public void testPacManCantMove() throws FileNotFoundException {
-                NoFrame frame = new NoFrame();
-                Map mp = frame.getMap();
-                Location pacLoc = new Location(1,1);
-              
-                Location down = pacLoc.shift(0, -1);
-                Location left = pacLoc.shift(-1, 0);
-                Location up = pacLoc.shift(0, 1);
-                Location right = pacLoc.shift(1, 0);
-
-                PacMan pm = new PacMan("Aditi", pacLoc, mp);
-                PacManComponent pacComp = new PacManComponent(pacLoc.x, pacLoc.y, 30);
-                mp.add("Aditi", pacLoc, pacComp, Map.Type.PACMAN);
-              
-                assertFalse(pm.move());
-        }
-
 }
