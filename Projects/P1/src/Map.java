c93ab651f2232da5ac31aeb6b87c5bf308b34d49
Josephine Palmhede <45825747+JosephinePalmhede@users.noreply.github.com>
2022-01-17 13:37:56 -0500

    Update Map.java

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -55,7 +55,7 @@ public class Map{
 
 	public boolean move(String name, Location loc, Type type) {
 		Location currLoc = locations.get(name);
-		locations.put(name, loc);
+		locations.replace(name, loc);
 		JComponent comp = (JComponent)components.get(name);
 		comp.setLocation(loc.x, loc.y);
 		if(field.get(currLoc)!= null) {
