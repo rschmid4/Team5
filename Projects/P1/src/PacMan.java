9a9cc406cbaf63bbf7054fb6f20fa0543cd40c12
Josephine Palmhede <45825747+JosephinePalmhede@users.noreply.github.com>
2022-01-17 12:28:37 -0500

    Update PacMan.java

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -72,7 +72,6 @@ public class PacMan{
 		if (this.myMap.getLoc(loc).contains(Map.Type.COOKIE)) {
 			return myMap.eatCookie("pacman");
 		}
- 		GhostComponent gc = new GhostComponent(5, 5, 10);
- 		return gc;
+ 		return null;
 	}
 }
