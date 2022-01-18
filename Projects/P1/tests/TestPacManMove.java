e3986f3e2df6085675eb5a8741a68545a8b8f665
unknown <Robert301301@gmail.com>
2022-01-17 19:16:58 -0500

    second fix for pacman move test

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -4,17 +4,15 @@ import java.io.*;
 
 public class TestPacManMove extends TestCase {
       public void testPacManMove() throws FileNotFoundException {
-                NoFrame frame = new NoFrame();
-                int size = 10;
-                Location pacLoc = new Location(5,5);
-                Map mp = frame.getMap();
+        NoFrame frame = new NoFrame();
+        Map mp = frame.getMap();
+        Location pacLoc = new Location(1,1);
+        Location down = pacLoc.shift(0, -1);
+        Location left = pacLoc.shift(-1, 0);
 
-                Location up = new Location (5, 4);
-                Location right = new Location(6, 5);
-
-                PacMan pm = new PacMan("Aditi", pacLoc, mp);
-
-                frame.addPacMan(pacLoc);
-                assertTrue(pm.move());
+        PacMan pm = new PacMan("Aditi", pacLoc, mp);
+        PacManComponent pacComp = new PacManComponent(pacLoc.x, pacLoc.y, 30);
+        mp.add("Aditi", pacLoc, pacComp, Map.Type.PACMAN);
+        assertTrue(pm.move());
         }
 }
